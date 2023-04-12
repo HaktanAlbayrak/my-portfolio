978da09fbc44753fd0189d59510c7d5646c574e1
Haktan Albayrak <78109456+HaktanAlbayrak@users.noreply.github.com>
2023-04-12 16:20:06 +0300

    page route done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import SiteLayout from './layout';
+import Home from './pages/home';
+import About from './pages/about';
+import Projects from './pages/projects';
+import { Route, Routes } from 'react-router-dom';
+import Page404 from './pages/404';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hi,There</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<SiteLayout />}>
+          <Route index={true} element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Projects' element={<Projects />} />
+        </Route>
+        <Route path='*' element={<Page404 />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/layout/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../assets/css/header.css';
+
+const Header = () => {
+  return (
+    <header className='container mx-auto flex flex-row justify-between p-4'>
+      <h1>Haktan Albayrak</h1>
+      <nav className='flex flex-row '>
+        <NavLink
+          to='/'
+          className='ml-6 hover:border-b-2 hover:border-b-gray-600 cursor-pointer transition-all text-xl tracking-widest font-semibold	'
+        >
+          HOME
+        </NavLink>
+        <NavLink
+          to='/About'
+          className='ml-6 hover:border-b-2 hover:border-b-gray-600 cursor-pointer transition-all text-xl tracking-widest font-semibold	'
+        >
+          ABOUT ME
+        </NavLink>
+        <NavLink
+          to='/Projects'
+          className='ml-6 hover:border-b-2 hover:border-b-gray-600 cursor-pointer transition-all text-xl tracking-widest font-semibold	'
+        >
+          PROJECTS
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
